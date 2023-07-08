c7f4ceef439596547cb36541852a32dde1c4bf82
Jirka Daněk <jirka.danek@jdanek.eu>
2023-07-08 13:56:52 +0200

    Move action registration and default config to plugin.json

--- plugins/extend/tinymce/class/TinymcePlugin.php
+++ plugins/extend/tinymce/class/TinymcePlugin.php
@@ -70,37 +70,4 @@ class TinymcePlugin extends ExtendPlugin
             'systemLang' => Core::$lang,
         ];
     }
-
-    /**
-     * ============================================================================
-     *  EXTEND CONFIGURATION
-     * ============================================================================
-     */
-
-    protected function getConfigDefaults(): array
-    {
-        return [
-            'editor_mode' => 'basic',
-            'mode_by_priv' => false,
-            // privileges
-            'priv_min_limited' => 1,
-            'priv_max_limited' => 500,
-            'priv_min_basic' => 600,
-            'priv_max_basic' => 1000,
-            'priv_min_advanced' => 10000,
-            'priv_max_advanced' => 10001,
-            // filemanager
-            'filemanager' => false,
-            'editor_in_boxes' => false,
-            'editor_in_perex' => true,
-        ];
-    }
-
-    public function getAction(string $name): ?PluginAction
-    {
-        if ($name === 'config') {
-            return new ConfigAction($this);
-        }
-        return parent::getAction($name);
-    }
 }
